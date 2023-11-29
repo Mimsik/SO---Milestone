6af0d4ccf6fb581da76e25ae25c91f1024807340
Cercelan Ariana <127732185+Mimsik@users.noreply.github.com>
2023-11-30 00:21:37 +0200

    s6.c

--- s6.c
+++ s6.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -10,7 +10,7 @@
 
 
 #pragma pack(1)
-typedef struct
+typedef struct BMP_Header
 {
     char signature[2];
     int32_t file_size;
@@ -27,22 +27,22 @@ typedef struct
     int32_t y_pixels;
     int32_t colors_used;
     int32_t colors_important;
-} BMPheader;
+
+} BMP_Header;
 #pragma pack()
 
 
-void get_permissions(char *str, mode_t mode)
+void get_permissions(char *array, mode_t mode)
 {
-    str[0] = (mode & S_IRUSR) ? 'R' : '-';
-    str[1] = (mode & S_IWUSR) ? 'W' : '-';
-    str[2] = (mode & S_IXUSR) ? 'X' : '-';
-    str[3] = (mode & S_IRGRP) ? 'R' : '-';
-    str[4] = (mode & S_IWGRP) ? 'W' : '-';
-    str[5] = (mode & S_IXGRP) ? 'X' : '-';
-    str[6] = (mode & S_IROTH) ? 'R' : '-';
-    str[7] = (mode & S_IWOTH) ? 'W' : '-';
-    str[8] = (mode & S_IXOTH) ? 'X' : '-';
-    str[9] = '\0';
+    array[0] = (mode & S_IRUSR) ? 'R' : '-';
+    array[1] = (mode & S_IWUSR) ? 'W' : '-';
+    array[2] = (mode & S_IXUSR) ? 'X' : '-';
+    array[3] = (mode & S_IRGRP) ? 'R' : '-';
+    array[4] = (mode & S_IWGRP) ? 'W' : '-';
+    array[5] = (mode & S_IXGRP) ? 'X' : '-';
+    array[6] = (mode & S_IROTH) ? 'R' : '-';
+    array[7] = (mode & S_IWOTH) ? 'W' : '-';
+    array[8] = (mode & S_IXOTH) ? 'X' : '-';
 }
 
 
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: %s <fisier_intrare>\n", argv[0]);
+        printf("Insufficient arguments");
         exit(-1);
     }
 
@@ -61,8 +61,8 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
-    BMPheader bmp_header;
-    if (read(input_file, &bmp_header, sizeof(BMPheader)) != sizeof(BMPheader))
+    BMP_Header bmp_header;
+    if (read(input_file, &bmp_header, sizeof(BMP_Header)) != sizeof(BMP_Header))
     {
         perror("Error reading BMP header");
         exit(-1);
@@ -71,45 +71,45 @@ int main(int argc, char *argv[])
     int width = bmp_header.width;
     int height = bmp_header.height;
 
-    char *fileName = strrchr(argv[1], '/');
-    if (fileName == NULL)
+    char *file_name = strrchr(argv[1], '/');
+    if (file_name == NULL)
     {
-        fileName = (char *)argv[1];
+        file_name = (char *)argv[1];
     }
     else
     {
-        fileName++;
+        file_name++;
     }
 
-    struct stat file_info;
-    if (fstat(input_file, &file_info) == -1)
+    struct stat file_data;
+    if (fstat(input_file, &file_data) == -1)
     {
         perror("Error getting file info");
         close(input_file);
-        return 1;
+        exit(-1);
     }
 
-    char mod_time[20];
-    strcpy(mod_time, (ctime(&file_info.st_mtime)));
+    char mod[20];
+    strcpy(mod, (ctime(&file_data.st_mtime)));
 
     int output_file = open("statistica.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     if (output_file == -1)
     {
-        perror("Error opening the output file");
+        perror("Error opening output file");
         close(input_file);
-        return 1;
+        exit(-1);
     }
 
     char permission1[10], permission2[10], permission3[10];
-    get_permissions(permission1, file_info.st_mode);
-    get_permissions(permission2, file_info.st_mode >> 3);
-    get_permissions(permission3, file_info.st_mode >> 6);
+    get_permissions(permission1, file_data.st_mode);
+    get_permissions(permission2, file_data.st_mode >> 3);
+    get_permissions(permission3, file_data.st_mode >> 6);
 
     char buffer[1024];
-    int numChars = sprintf(buffer, "nume fisier: %s\ninaltime: %d\nlungime: %d\ndimensiune: %lu\nidentificatorul Utilizatorului: %d\ntimpul ultimei modificari: %scontorul de legaturi: %lu\ndrepturi de acces user: %s\ndrepturi de acces grup: %s\ndrepturi de acces altii: %s\n",
-                           fileName, height, width, (unsigned long)file_info.st_size, file_info.st_uid, mod_time, (unsigned long)file_info.st_nlink, permission1, permission2, permission3);
+    int count = sprintf(buffer, "nume fisier: %s\ninaltime: %d\nlungime: %d\ndimensiune: %lu\nidentificatorul Utilizatorului: %d\ntimpul ultimei modificari: %scontorul de legaturi: %lu\ndrepturi de acces user: %s\ndrepturi de acces grup: %s\ndrepturi de acces altii: %s\n",
+                           file_name, height, width, (unsigned long)file_data.st_size, file_data.st_uid, mod, (unsigned long)file_data.st_nlink, permission1, permission2, permission3);
 
-    write(output_file, buffer, numChars);
+    write(output_file, buffer, count);
     close(input_file);
     close(output_file);
 
